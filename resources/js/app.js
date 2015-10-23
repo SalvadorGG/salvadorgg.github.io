138ecb419a0bb9068a28085ca637ab7bda798520
Salvador Gonzalez Garcia <salvador@salvadorgg.me>
2015-10-23 16:18:28 -0400

    Initial Setup
    Add image and some text.

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+/*
+var pattern = Trianglify({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    variance: "1",    
+    x_colors: 'Blues',
+    cell_size: 40
+        
+});
+
+var banner = document.getElementById('banner')
+banner.appendChild(pattern.svg());
+
+$(document).ready(pattern);
+*/
